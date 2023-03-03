2e50840f0222e5f5ec36550798b053765b73e250
Marcus <marcusfoin1@gmail.com>
2023-03-03 21:34:20 +0100

    Maj Test filtreH

--- src/test/scala/TestsFiltreH.scala
+++ src/test/scala/TestsFiltreH.scala
@@ -1,11 +1,11 @@
-package TP345
+
 import org.junit.Test
 import org.junit.Assert._
 import application.FiltrageHTML._
 import library._
 
 
-class Tests {
+class TestsFiltreH {
 
   @Test
   def Test1_filtreHtml(): Unit = {
@@ -46,6 +46,7 @@ class Tests {
           Tag("a", List(("href", "http://www.irisa.fr")),
             List(Texte("Lien"))),Texte("voiture de luxe."))))))),Mot("luxe")))
   }
+  @Test
   def Test4_filtreHtml(): Unit = {
     assert(filtreHtml(Tag("html", List(),
     List(
@@ -84,4 +85,45 @@ class Tests {
           Tag("a", List(("href", "http://www.irisa.fr")),
             List(Texte("Lien"))),Texte("voiture de luxe."))))))),Et(Mot("voiture"),Mot("luxe"))))
   }  
+
+@Test
+  def Test1_contientMotCle(): Unit = {
+    assert(contientMotCle(Tag("html", List(),
+    List(
+      Tag("head", List(),
+        List(
+          Tag("meta", List(("charset", "utf-8")), List()),
+          Tag("title", List(), List(Texte("My Page"))))),
+      Tag("body", List(), List(
+        Tag("center", List(), List(
+          Tag("a", List(("href", "http://www.irisa.fr")),
+            List(Texte("Lien"))),Texte("Test1 teste0 et 5"))))))),"Test1"))
+  }  
+  @Test
+  def Test2_contientMotCle(): Unit = {
+    assert(!contientMotCle(Tag("html", List(),
+    List(
+      Tag("head", List(),
+        List(
+          Tag("meta", List(("charset", "utf-8")), List()),
+          Tag("title", List(), List(Texte("My Page"))))),
+      Tag("body", List(), List(
+        Tag("center", List(), List(
+          Tag("a", List(("href", "http://www.irisa.fr")),
+            List(Texte("Lien"))),Texte("Test21 et testswd40"))))))),"Test1"))
+  }  
+  @Test
+  def Test3_contientMotCle(): Unit = {
+    assert(!contientMotCle(Tag("html", List(),
+    List(
+      Tag("head", List(),
+        List(
+          Tag("meta", List(("charset", "utf-8")), List()),
+          Tag("title", List(), List(Texte("My Page"))))),
+      Tag("body", List(), List(
+        Tag("center", List(), List(
+          Tag("a", List(("href", "http://www.irisa.fr")),
+            List(Texte("Lien"))),Texte("voiture de luxe."))))))),"lien"))
+  }
+
 }
