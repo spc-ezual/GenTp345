1db80d273a88e687b62fd9f7d495c2e83808f9dd
Marcus <marcusfoin1@gmail.com>
2023-03-03 21:02:29 +0100

    Modif de fichié

--- src/main/scala/application/HTMLToString.scala
+++ src/main/scala/application/HTMLToString.scala
@@ -6,9 +6,10 @@ object HTMLToString extends HtmlVersString {
   def traduire(html: Html): String = {
     html match {
       case Tag(name, attributes, children) =>
-        val attrString = attributes.map { case (key, value) => s"""$key="$value"""" }.mkString(" ")
-        val childrenString = children.map(traduire).mkString("\n")
-        s"<$name $attrString>\n$childrenString\n</$name>\n"
+        val attrString =
+          attributes.map { case (key, value) => s"$key=$value" }.mkString(" ")
+        val childrenString = children.map(traduire).mkString
+        s"<$name $attrString>$childrenString \n </$name>"
       case Texte(content) =>
         content
     }
